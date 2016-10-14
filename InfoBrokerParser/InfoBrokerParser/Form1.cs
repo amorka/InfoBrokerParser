86673ad26d6ea8f871ff0152cdc40d58a7218a75
Roman Miasnikov2 <amorka89@gmail.com>
2016-10-14 10:55:13 +0300

    Создан проект

--- InfoBrokerParser/InfoBrokerParser/Form1.cs
+++ InfoBrokerParser/InfoBrokerParser/Form1.cs
@@ -16,5 +16,10 @@ namespace InfoBrokerParser
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
